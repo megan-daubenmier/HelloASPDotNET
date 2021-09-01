fe2584371916781a3fc2253ef9058000c15d41b6
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-08-31 21:02:20 -0500

    Completed exercises - but I have questions (why did it say to use public static method?? Couldn't get it to work with public static so switched to public CreateMessage.)

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -6,23 +6,59 @@ using System.Threading.Tasks;
 
 namespace HelloASPDotNET.Controllers
 {
+    [Route("/helloworld")]
     public class HelloController : Controller
     {
         // GET: /<controller>/
         [HttpGet]
-        [Route("/helloworld/")]
         public IActionResult Index()
         {
-            string html = "<h1>Hello World!</h1>";
+            string html = "<form method = 'post' action = '/helloworld'>" 
+                + "<input type='text' name='name'/>" 
+                + "<select name='language'>"
+                + "<option value='English'>" + "English" + "</option>"
+                + "<option value='Spanish'>" + "Spanish" + "</option>"
+                + "<option value='French'>" + "French" + "</option>"
+                + "<option value='Swahili'>" + "Swahili" + "</option>"
+                + "<option value='German'>" + "German" + "</option>"
+                + "</select>"
+                + "<input type='submit' value='Greet Me!'/>"
+                +"</form>";
             return Content(html, "text/html");
         }
 
-        //GET: hello/welcome
-        [HttpGet]
-        [Route("/helloworld/welcome/{name?}")]
+        [HttpGet("{name?}/{language?}")]
+        [HttpPost]
+        public IActionResult CreateMessage(string name = "World", string language = "English")
+        {
+            if(language == "English")
+            {
+                return Content("<h1>Hello, " + name + "!</h1", "text/html");
+            } else if (language == "Spanish")
+            {
+                return Content("<h1>Hola, " + name + "!</h1", "text/html");
+            } else if (language == "French")
+            {
+                return Content("<h1>Bonjour, " + name + "!</h1", "text/html");
+            } else if (language == "Swahili")
+            {
+                return Content("<h1>Habari, " + name + "!</h1", "text/html");
+            } else if (language == "German")
+            {
+                return Content("<h1>Hallo, " + name + "!</h1", "text/html");
+            }
+            else
+            {
+                return Content("<h1></h1>", "text/html");
+            }
+        }
+
+        /*GET: hello/welcome
+        [HttpGet("welcome/{name?}")]
+        [HttpPost]
         public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
-        }
+        }*/
     }
 }
